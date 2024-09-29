d7735e73ca0de0f998623c15f0617388f59bebee
Geethapranay1 <geethapranay.official@gmail.com>
2024-09-29 16:52:00 +0530

    added space

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -11,6 +11,9 @@ import { redirect } from "next/navigation";
 
 
 export default async function Home() {
+
+
+  
   const session = await getServerSession(authOptions)
   if(session?.user) {
     redirect("/dashboard")
