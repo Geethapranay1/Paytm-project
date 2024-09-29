cdf3143392f44c8483549b5672d8a37feeec0f26
Geethapranay1 <geethapranay.official@gmail.com>
2024-09-29 16:54:14 +0530

    removed

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,10 +1,9 @@
 
 import { Appbar } from "@repo/ui/Appbar";
 import { getServerSession } from "next-auth";
-import { getSession, signIn, signOut, useSession } from "next-auth/react";
+
 import { authOptions } from "./lib/auth";
 import { redirect } from "next/navigation";
-
 
 
 
@@ -13,7 +12,7 @@ import { redirect } from "next/navigation";
 export default async function Home() {
 
 
-  
+
   const session = await getServerSession(authOptions)
   if(session?.user) {
     redirect("/dashboard")
