28bff955eb7da714e15d72c6715a68811a230485
Geethapranay1 <geethapranay.official@gmail.com>
2024-09-29 17:14:02 +0530

    fixed

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -4,14 +4,13 @@ import { authOptions } from "./lib/auth";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
+
   const session = await getServerSession(authOptions);
   if(session?.user) {
     redirect("/dashboard")
   } else {
     redirect("/api/auth/signin")
   }
+  
 }
-
-
-
 
